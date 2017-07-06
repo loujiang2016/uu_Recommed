3be6cfd2bfef119cc0d93543c2b6622bf2803e76
loujiang351 <283489706@qq.com>
2017-07-06 15:02:08 +0800

    uu_recommed_Sdk

---
+++ UU_Recommed_SDK/UU_Recommed_SDK/UU_Recommed_SDK.h
@@ -0,0 +1,21 @@
+//
+//  UU_Recommed_SDK.h
+//  UU_Recommed_SDK
+//
+//  Created by Jiang Lou on 2017/7/6.
+//  Copyright © 2017年 Jiang Lou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UU_Recommed_SDK : NSObject
+
+
+new file:   UU_Recommed_SDK/UU_Recommed_SDK.xcodeproj/project.pbxproj
+new file:   UU_Recommed_SDK/UU_Recommed_SDK.xcodeproj/project.xcworkspace/contents.xcworkspacedata
+new file:   UU_Recommed_SDK/UU_Recommed_SDK.xcodeproj/project.xcworkspace/xcuserdata/Jiang.xcuserdatad/UserInterfaceState.xcuserstate
+new file:   UU_Recommed_SDK/UU_Recommed_SDK.xcodeproj/xcuserdata/Jiang.xcuserdatad/xcschemes/UU_Recommed_SDK.xcscheme
+new file:   UU_Recommed_SDK/UU_Recommed_SDK.xcodeproj/xcuserdata/Jiang.xcuserdatad/xcschemes/xcschememanagement.plist
+new file:   UU_Recommed_SDK/UU_Recommed_SDK/UU_Recommed_SDK.h
+new file:   UU_Recommed_SDK/UU_Recommed_SDK/UU_Recommed_SDK.m
+@end
---
+++ UU_Recommed_SDK/UU_Recommed_SDK/UU_Recommed_SDK.m
@@ -0,0 +1,13 @@
+//
+//  UU_Recommed_SDK.m
+//  UU_Recommed_SDK
+//
+//  Created by Jiang Lou on 2017/7/6.
+//  Copyright © 2017年 Jiang Lou. All rights reserved.
+//
+
+#import "UU_Recommed_SDK.h"
+
+@implementation UU_Recommed_SDK
+
+@end
